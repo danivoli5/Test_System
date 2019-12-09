180a47ddb1dadfd34b75a79c5744e1bd1dffca5d
danivoli5 <57321488+danivoli5@users.noreply.github.com>
2019-12-09 10:36:28 +0200

    Add files via upload

---
+++ course.cpp
@@ -0,0 +1,6 @@
+//#include "course.h"
+//
+//Courses::Courses(Details courses2)
+//{
+//	courses.push_back(courses2);
+//}
---
+++ course.h
@@ -0,0 +1,17 @@
+//#pragma once
+//#include <direct.h>
+//#include <string>
+//#include <iostream>
+//using namespace std;
+//#include"Details.h"
+//#include<vector>
+//class Courses
+//{
+//protected:
+//	vector<Details>courses;
+//
+//
+//public:
+//	Courses(Details courses2);
+//};
+//
