6abd085512e9701a38c1ceda91cc63d85827c74b
erelle <57131672+erelle@users.noreply.github.com>
2020-01-16 21:17:22 +0200

    Update main_including_log.cpp

--- main_including_log.cpp
+++ main_including_log.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #include<Windows.h>
 #include"Details.h"
 #include "Teacher.h"
-int Add_q(string name2, string semester2, string lecturer2, string subjects2, int year2, int difficulty2, string due, int length)
+int Add_q(string name2, string semester2, string lecturer2, string subjects2, int year2, int difficulty2, string due)
 {
 
 	cout << "Press enter when you ready to crop, In the end press CTRL+V in the word.doc" << endl;
@@ -18,8 +18,8 @@ int Add_q(string name2, string semester2, string lecturer2, string subjects2, in
 	keybd_event(0x53, 0, 0, 0);
 	keybd_event(NULL, NULL, NULL, NULL);
 	keybd_event(0, 0, 0, 0);
-	Details temp(name2, semester2, lecturer2, subjects2, year2, difficulty2, due, length);
-	temp.openInfo(name2, semester2, lecturer2, subjects2, year2, difficulty2, due, length);
+	Details temp(name2, semester2, lecturer2, subjects2, year2, difficulty2, due);
+	temp.openInfo(name2, semester2, lecturer2, subjects2, year2, difficulty2, due);
 	return 0;
 }
 
@@ -31,7 +31,7 @@ void questions(int num)
 	stuff.open("new_test.txt");
 	fin.open("question.txt", ios::in);
 	char str[20];
-	char my_character,num2;
+	char my_character, num2;
 
 	while (!fin.eof()) {
 
@@ -47,9 +47,9 @@ void questions(int num)
 				}
 			}
 		}
-		
+
 	}
-	
+
 }
 
 int main()
@@ -58,11 +58,9 @@ int main()
 	log.open("file_log.txt");//log
 	time_t my_time = time(NULL);
 	log << "starting runing time in nano seconds: " << my_time << endl;
-	int logCounter=0;
-	ofstream coor, student, teach,quest;
-
-	
-
+	int logCounter = 0;
+	ofstream coor, student, teach, quest;
+	ifstream read;
 	quest.open("question.txt", ios_base::app);
 	quest.close();
 	coor.open("coordinators.txt", ios_base::app);
@@ -71,6 +69,7 @@ int main()
 	student.close();
 	teach.open("teacher.txt", ios_base::app);
 	teach.close();
+
 	int person, index, index2, size1, size2;
 	Details all;
 	Details temp;
@@ -82,39 +81,31 @@ int main()
 	long phone, pass, password;
 	do
 	{
-		cout << "who are you? enter a number: \n 1.coordinator, 2.proffesor, 3.student,0.exit\n";
+		cout << "\nwho are you? enter a number: \n 1.coordinator, 2.proffesor, 3.student,0.exit\n";
 		cin >> person;
 		switch (person)
 		{
 		case 1:
 			log << "coordinator function runing " << endl;//log file runing
 			logCounter++;
-			cout << "choose:\n1.enter a new teacher, 2.remove a teacher, 3.enter to your account, 4.creat an account" << endl;
-			cout << "5.add a new student, 6. remove a student" << endl;
+			cout << "choose 1 to create a new account or 2 to enter to your account" << endl;
 			cin >> index;
 			if (index == 1)
 			{
-				log << "adding a new teacher function runing " << endl;//log file writing
+				log << "creating coordinator account function runing " << endl;//writing into log file
 				logCounter++;
-				cout << "enter the name: ";
+				cout << "enter your name : " << endl;
 				cin >> Pname;
-				cout << "\nenter phone number: ";
-				cin >> phone;
-				cout << "\nenter subject: ";
-				cin >> sub;
-				cout << "\n enter password: ";
-				cin >> pass;
-				Teacher teacher(Pname, sub, phone, pass);
-				teach.open("teacher.txt", ios::app);
-				teach << Pname << endl;
-				teach << pass << endl;
-				teach.close();
+				cout << "enter password : " << endl;
+				cin >> tempPass;
+				coor.open("coordinators.txt", ios::app);
+				coor << Pname << endl;
+				coor << tempPass << endl;
+				coor.close();
+				tempStr = Pname + ' ' + tempPass;
+				coordinators.push_back(tempStr);
 			}
 			if (index == 2)
-			{
-				//remove a teacher
-			}
-			if (index == 3)
 			{
 				log << "login function runing " << endl;//writing to log file
 				logCounter++;
@@ -128,7 +119,7 @@ int main()
 						k++;
 					}
 				}
-				if (k != 1)
+				if (k == 0)
 				{
 					log << "a wrong input was entered." << endl;//log file runing
 					logCounter++;
@@ -137,128 +128,152 @@ int main()
 				}
 				do
 				{
-					cout << "enter a number: 0. exit 1.add an answer 2.delete a question 3.add a question 4. open a question\n";
+					cout << "enter a number: 0. exit 1.delete a question 2. add a question 3. open a question 4. enter a new teacher 5. add a new student";
 					cin >> num;
 					switch (num)
 					{
 					case 1:
-						//add an answer
-						break;
-
-					case 2:
+					{
 						log << "function to delete a question was used." << endl;//log file runing
 						logCounter++;
-						cout << "enter the name, semester, lecturer, subjects, due, year , difficulty and question length" << endl;
-						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty >> length;
-						tempStr = name + ' ' + to_string(year) + ' ' + semester + ' ' + due + ' ' + lecturer + ' ' + subjects + ' ' + to_string(difficulty) + ' ' + to_string(length) + ' ' + ".docx";
+						cout << "enter the name, semester, lecturer, subjects, due, year , difficulty" << endl;
+						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+						tempStr = name + ' ' + to_string(year) + ' ' + semester + ' ' + due + ' ' + lecturer + ' ' + subjects + ' ' + to_string(difficulty) + ' ' + ".docx";
 						if (remove(tempStr.c_str()) != 0)
 							perror("Error deleting file");
 						else
 							puts("File successfully deleted");
 						file2.close();
 						break;
-
-					case 3:
+					}
+					case 2:
+					{
 						log << "function to add a question was used." << endl;//log file runing
 						logCounter++;
-						cout << "enter the name, semester, lecturer, subjects, due, year difficulty and question length" << endl;
-						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty, length;
-						tempStr = name + ' ' + to_string(year) + ' ' + semester + ' ' + due + ' ' + lecturer + ' ' + subjects + ' ' + to_string(difficulty) + ' ' + to_string(length) + ' ' + ".docx";
-						temp.addDetails(name, semester, lecturer, subjects, year, difficulty, due, length);
+						cout << "enter the name, semester, lecturer, subjects, due, year difficulty" << endl;
+						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+						tempStr = name + ' ' + to_string(year) + ' ' + semester + ' ' + due + ' ' + lecturer + ' ' + subjects + ' ' + to_string(difficulty) + ' ' + ".docx";
+						temp.addDetails(name, semester, lecturer, subjects, year, difficulty, due);
 						all.addDetails(temp);
 						quest.open("question.txt", ios::app);
 						quest << tempStr << endl;
 						quest.close();
-						//x=Add_q(name, semester, lecturer, subjects, year, difficulty, due,length);
-						//if(x==0)
-						break;
-					case 4:
-						log << "opening the question file requested." << endl;//log file runing
-						logCounter++;
-						cout << "enter the name, semester, lecturer, subjects, due, year and difficulty and question length" << endl;
-						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty, length;
-						all.openInfo(name, semester, lecturer, subjects, year, difficulty, due, length);
+						//Add_q(name, semester, lecturer, subjects, year, difficulty, due);
 						break;
 					}
+					case 3:
+					{
+						log << "opening the question file requested." << endl;//log file runing
+						logCounter++;
+						cout << "enter the name, semester, lecturer, subjects, due, year and difficulty" << endl;
+						cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+						all.openInfo(name, semester, lecturer, subjects, year, difficulty, due);
+						break;
+					}
+					case 4:
+					{
+						log << "adding a new teacher function runing " << endl;//log file writing
+						logCounter++;
+						cout << "enter the name: ";
+						cin >> Pname;
+						cout << "\nenter phone number: ";
+						cin >> phone;
+						cout << "\nenter subject: ";
+						cin >> sub;
+						cout << "\n enter password: ";
+						cin >> pass;
+						Teacher teacher(Pname, sub, phone, pass);
+						Teacher tempTeach(teacher);
+						teach.open("teacher.txt", ios::app);
+						teach << Pname << endl;
+						teach << pass << endl;
+						teach.close();
+						break;
+					}
+					case 5:
+					{
+						log << "function to add a new student " << endl;//writing to log file
+						logCounter++;
+						cout << "enter name and password" << endl;
+						cin >> Pname >> pass;
+						student.open("students.txt", ios::app);
+						student << Pname <<endl<< pass<<endl;
+						student.close();
+						break;
 
-				} while (num != 0);
-			}
-			if (index == 4)
-			{
-				log << "log in coordinator account function runing " << endl;//writing into log file
-				logCounter++;
-				cout << "enter your name : " << endl;
-				cin >> Pname;
-				cout << "enter password : " << endl;
-				cin >> tempPass;
-				coor.open("coordinators.txt", ios::app);
-				coor << Pname << endl;
-				coor << tempPass << endl;
-				coor.close();
-				tempStr = Pname + ' ' + tempPass;
-				coordinators.push_back(tempStr);
-			}
-			if (index == 5)
-			{
-				log << "function to add a new student " << endl;//writing to log file
-				logCounter++;
-				cout << "enter name and password" << endl;
-				cin >> Pname >> pass;
-				student.open("students.txt", ios::app);
-			}
-			if (index == 6)
-			{
-				//remove student
+					}
+
+					}
+				}while (num != 0);
 			}
 			break;
-			
-		case 2:
-			cout << "enter 0 to exit, enter 1 to search a question, enter 2 to add a question, 3. build new test by length of question" << endl;
+        case 2:
+			cout << "enter your name and password" << endl;
+			cin >> Pname;
+			cin >> tempPass;
+			read.open("teacher.txt", ios::app);
+			tempStr = "";
+			while (!read.eof()) {
+				while (read >> name && name != "\n")
+				{
+					tempStr += name;
+				}
+				if (tempStr == Pname)
+				{
+					read.eof();
+					goto continueTo;
+				}
+			}
+			cout << "no matching password or name, please try again" << endl;
+			break;
+			continueTo:
+			cout << "enter 0 to exit, enter 1 to search a question, enter 2 to add a question" << endl;
 			cin >> index2;
 			if (index2 == 0)
 				break;
 			else if (index2 == 1) {
 				log << "function to search a question runings." << endl;//log file runing
 				logCounter++;
-				cout << "enter the name, semester, lecturer, subjects, due, year, difficulty and question length" << endl;
-				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty >> length;
-				all.openInfo(name, semester, lecturer, subjects, year, difficulty, due, length);
+				cout << "enter the name, semester, lecturer, subjects, due, year, difficulty" << endl;
+				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+				all.openInfo(name, semester, lecturer, subjects, year, difficulty, due);
 				break;
 			}
 			else if (index2 == 2) {
 				log << "function to add a question runings." << endl;//log file runing
 				logCounter++;
-				cout << "enter the name, semester, lecturer, subjects, due, year difficulty and question length" << endl;
-				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty>> length;
-				temp.addDetails(name, semester, lecturer, subjects, year, difficulty, due, length);
+				cout << "enter the name, semester, lecturer, subjects, due, year difficulty" << endl;
+				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+				temp.addDetails(name, semester, lecturer, subjects, year, difficulty, due);
 				all.addDetails(temp);
-				x = Add_q(name, semester, lecturer, subjects, year, difficulty, due, length);
+				x = Add_q(name, semester, lecturer, subjects, year, difficulty, due);
 				break;
 			}
-			else if (index2 == 3) {
-				cout << "enter a length:" << endl;
-				cin >> num;
-				questions(num);
-				break;
-			}
+			break;
 		case 3:
 			cout << "enter 0 to exit, enter 1 to search a question" << endl;
 			cin >> index2;
 			if (index2 == 0)
 				break;
 			else if (index2 == 1) {
-				cout << "enter the name, semester, lecturer, subjects, due, year, difficulty and question length" << endl;
-				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty >> length;
-				all.openInfo(name, semester, lecturer, subjects, year, difficulty, due, length);
+				cout << "enter the name, semester, lecturer, subjects, due, year, difficulty" << endl;
+				cin >> name >> semester >> lecturer >> subjects >> due >> year >> difficulty;
+				all.openInfo(name, semester, lecturer, subjects, year, difficulty, due);
 			}
 			break;
 		}
-	} while (person != 0);
+	}
+
+	while (person != 0);
 
 	time_t my_time2 = time(NULL);
 	log << "ending runing time in nano seconds: " << my_time2 << endl;// print to log file
 	log << "total run time: " << my_time2 - my_time << endl;//print to log file
 	log << "total log usage: " << logCounter << endl;
+	cout << "bye bye";
 	log.close();// closing of log file
 	return 0;
 }
+
+
+
