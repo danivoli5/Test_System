f3cb3f32606a76a700f259bb2017c4144aca74fa
erelle <57131672+erelle@users.noreply.github.com>
2020-01-16 21:19:24 +0200

    Update Teacher.cpp

--- Teacher.cpp
+++ Teacher.cpp
@@ -2,7 +2,7 @@
 
 long Teacher::getPhone()
 {
-	 return this->phone;
+	return this->phone;
 }
 
 long Teacher::getPass()
@@ -20,35 +20,34 @@ string Teacher::getSub()
 	return this->sub;
 }
 
- Teacher::Teacher(Teacher const& prof)
+Teacher::Teacher(Teacher const& prof)
 {
 	password = prof.password;
 	phone = prof.phone;
 	sub = prof.sub;
 	name = prof.name;
+	professors.push_back(prof);
 }
 
- Teacher::Teacher(string name2, string subject, long phone2, int pass)
-:name(name2), sub(subject), phone(phone2), password(pass)
- {
- }
+Teacher::Teacher(string name2, string subject, long phone2, int pass)
+	:name(name2), sub(subject), phone(phone2), password(pass)
+{
+}
 
- void Teacher::addTeacher(Teacher const& prof)
- {
-	 this->password = prof.password;
-	 this->phone = prof.phone;
-	 this->sub = prof.sub;
-	 this->name = prof.name;
- }
+void Teacher::addTeacher(Teacher const& prof)
+{
+	this->password = prof.password;
+	this->phone = prof.phone;
+	this->sub = prof.sub;
+	this->name = prof.name;
+}
 
- void Teacher::addTeacher(string name2, string subject, long phone2, int pass)
- {
-	 Teacher prof(name2, subject, phone2, pass);
-	 this->professors.push_back(prof);
-	 this->password = pass;
-	 this->phone =phone2;
-	 this->sub = subject;
-	 this->name =name2;
- }
-
-
+void Teacher::addTeacher(string name2, string subject, long phone2, int pass)
+{
+	Teacher prof(name2, subject, phone2, pass);
+	this->professors.push_back(prof);
+	this->password = pass;
+	this->phone = phone2;
+	this->sub = subject;
+	this->name = name2;
+}
